0668afce5ce59255faaac8193c65c9f387628fbc
Fritzy-gif <fserrano@parrastu.catholic.edu.au>
2020-06-23 12:44:31 +1000

    fixed the JS linkage

--- pac.js
+++ pac.js
@@ -6,22 +6,22 @@ var lives = 3;
 //create fns//
 function pacmanControls() {
     if (keyWentDown("right")) {
-      pacman.velocityX = 4;
+      pacman.velocityX = 5;
       pacman.velocityY = 0;
       pacman.setAnimation("pacmanR");
     }
     if (keyWentDown("left")) {
-      pacman.velocityX = -4;
+      pacman.velocityX = -5;
       pacman.velocityY = 0;
       pacman.setAnimation("pacmanL");
     }
     if (keyWentDown("up")) {
-      pacman.velocityY = -4;
+      pacman.velocityY = -5;
       pacman.velocityX = 0;
       pacman.setAnimation("pacmanU");
     }
     if (keyWentDown("down")) {
-      pacman.velocityY = 4;
+      pacman.velocityY = 5;
       pacman.velocityX = 0;
       pacman.setAnimation("pacmanD");
     }
